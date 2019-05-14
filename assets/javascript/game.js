471768b7e8c55439f300781471d1cb8205420f95
Your Name <you@example.com>
2019-05-14 17:14:08 -0400

    photos, buttons and some divs

---
+++ assets/javascript/game.js
@@ -0,0 +1,83 @@
+
+// Option 1 Game design notes
+// The random number shown at the start of the game should be between 19 - 120.
+// Each crystal should have a random hidden value between 1 - 12.
+
+
+// create variables
+// generate random number (target)
+// value of button clicked
+// when clikc update the score
+// wins when  target num is met
+// lost if goes + than target num
+// win/lose game restarts
+// when restarted a new random num generates, scores back to zero.
+// show num of games played
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Variables
+// parisPic = 0
+// bcnPic = 0
+// cphPic = 0
+// nicePic = 0
+// targetNum = 0
+// wins = 0
+// lost = 0
+
+
+// function start(){
+//    // generate random number
+//    //intialize all shoe values
+//    //shoeVal1 = Math.floor(Math.random()*101) +19
+//    $("#totalNumber").text("0")
+// }
+
+// function start(){
+// var randomNum = Math.floor(Math.random()*101)+19;
+
+// }
+
+
+// $("#id1")
+
+// $("#id1").on("click", function(){
+//    guesses = guesses + shoeval1
+//    $("#totalNumber").text(guesses)
+// play()
+
+// })
+// $("#id2").on("click", function(){
+//    guesses = guesses + shoeval3
+// })
+// $("#id3").on("click", function(){
+//    guesses = guesses + shoeval3
+
+// })
+
+
+// play()
+// if (guesses == randomNumber){
+//     wins += 1;
+// } else (guesses !== randomNumber){
+//     lost += 1;
+// }
+
+// start()
+
+
